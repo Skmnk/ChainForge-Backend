54a2518477dacc3df976d5715ccae7dc099ba365
Skmnk <dreamsrikrishna7@gmail.com>
2024-09-04 18:47:51 +0530

    Add endpoint

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://chain-forge-forntend-byo93l4mc-skmnks-projects.vercel.app/',  // Replace with your frontend's URL
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
-// app.use('/api/auth', authRoutes         );
+app.use('/api/auth', authRoutes         );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
