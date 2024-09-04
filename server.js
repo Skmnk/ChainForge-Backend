12cdb3536f059d76de867aca9dde69b5a67ca0e7
Skmnk <dreamsrikrishna7@gmail.com>
2024-09-04 13:58:55 +0530

    Add endpoint

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://your-frontend-service.vercel.app',  // Replace with your frontend's URL
+    credentials: true,
+}));
 app.use(express.json());
 
-app.use('/api/auth', authRoutes         );
+// app.use('/api/auth', authRoutes         );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
